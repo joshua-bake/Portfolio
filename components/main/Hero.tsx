89bcb61d845aec7f77e21edcd9f11827d79c4d0d
Joshua Baker <joshua.jbaker12@icloud.com>
2024-05-19 01:41:39 -0400

    added skills section

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -3,15 +3,15 @@ import HeroContent from '../sub/HeroContent'
 
 const Hero = () => {
     return (
-        <div className='relative flex flex-col h-full w-full'>
+        <div className='relative flex flex-col h-full w-full' id='about-me'>
             <video
-            autoPlay
-            muted
-            loop
-            className='rotate-180 absolute-top-[-340px] left-0 z-[1] w-full h-full object-cover'>
+                autoPlay
+                muted
+                loop
+                className='rotate-180 absolute-top-[-340px] left-0 z-[1] w-full h-full object-cover'>
                 <source src='/blackhole.webm' type='video/webm' />
             </video>
-        <HeroContent/>
+            <HeroContent />
         </div>
     )
 }
